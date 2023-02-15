52ff13f2b847a857e7be2a5a548af4bf4e8e4980
alex <lambin1483alex@gmail.com>
2023-02-15 17:55:39 +0300

    Add scroll to buttons

--- headers/selectwidget.h
+++ headers/selectwidget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QPushButton>
+#include <QButtonGroup>
+#include <QScrollArea>
 #include <QVBoxLayout>
 #include <QVector>
 
--- sources/selectwidget.cpp
+++ sources/selectwidget.cpp
@@ -1,19 +1,30 @@
 #include "../headers/selectwidget.h"
 
 SelectWidget::SelectWidget(unsigned int cores, QWidget *parent) : QWidget(parent) {
+    QScrollArea *scrollArea = new QScrollArea(this);
+
+    QWidget *buttonWidget = new QWidget(scrollArea);
     for (unsigned int i = 0; i < cores + 1; ++i) {
         QString name = "ЦП " + (i == 0 ? "(общий)" : QString::number(i - 1));
-        QPushButton *button = new QPushButton(name, this);
+        QPushButton *button = new QPushButton(name, buttonWidget);
+        button->setMinimumWidth(buttonWidget->width());
         buttons.append(button);
     }
     curButtonID = 0;
     buttons[curButtonID]->setEnabled(false);
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    QVBoxLayout *buttonLayout = new QVBoxLayout(this);
     for (auto *button : buttons) {
-        layout->addWidget(button);
+        buttonLayout->addWidget(button);
     }
-    layout->setAlignment(Qt::AlignTop);
+    buttonLayout->setAlignment(Qt::AlignTop);
+    buttonWidget->setLayout(buttonLayout);
+    scrollArea->setWidget(buttonWidget);
+    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(scrollArea);
     setLayout(layout);
 
     for (unsigned int i = 0; i < cores + 1; ++i) {
